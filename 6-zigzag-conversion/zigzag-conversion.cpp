cf2d59622163f64be37df177ab256476edb6ac39
Angrej Singh <engg.angrejsingh@gmail.com>
2026-07-07 22:49:41 +0530

    Time: 2 ms (81.28%) | Memory: 10.9 MB (90.98%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,55 +1,28 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        int sizeofstring = s.length();
-        // If the string has only one character, return it
-        if (sizeofstring == 1)
-            return s;
+        // handle single row
+        if (numRows == 1) return s;
 
-        // If there is only one row, Zigzag is the same as the original string
-        if (numRows == 1)
-            return s;
+        string ret_s = "";
+        for (int i = 0; i < numRows; i++) {
+            int index = i;
+            bool top_row = (i == 0);
+            bool last_row = (i == numRows - 1);
+            bool dir = !last_row; // true: down, false: up
 
-        // Create a string for each row
-        vector<string> rows(numRows);
+            while (index < s.length()) {
+                ret_s += s[index];
 
-        int index = 0;
-
-        while (index < sizeofstring) {
-
-            // Move downward
-            for (int row = 0; row < numRows && index < sizeofstring; row++) {
-                rows[row].push_back(s[index++]);
-            }
-
-            // Move diagonally upward
-            for (int row = numRows - 2; row > 0 && index < sizeofstring; row--) {
-                rows[row].push_back(s[index++]);
+                if (dir) {
+                    index += 2*(numRows - i - 1);
+                } else {
+                    index += 2*i;
+                }
+                
+                if (!(last_row || top_row)) dir = !dir;
             }
         }
-
-        // Combine all rows
-        string result = "";
-
-        for (string row : rows) {
-            result += row;
-        }
-
-        return result;
+        return ret_s;
     }
 };
-
-/*
-Time Complexity: O(n)
-
-Reason:
-- We visit each character of the string exactly once.
-- Every character is added to one row.
-- Therefore, the total number of operations is proportional to the length of the string.
-
-Space Complexity: O(n)
-
-Reason:
-- We create extra strings to store the Zigzag rows.
-- Together, these rows store all characters of the input string.
-*/
